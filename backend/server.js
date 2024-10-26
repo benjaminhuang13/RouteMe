cd045d2117432890e9c0d48cf22bb6c23deb8b8d
Benjamin Huang <benjamin.huang@alumni.stonybrook.edu>
2024-10-26 17:23:28 -0400

    update

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,8 @@ app.use(cors()); //use middleware
 app.use(express.json()); //allows server to accept json in a body of a request
 
 app.use("/api/v1/customer_data", customer_data); //url of the routes
-app.use("*", (req, res) => res.status(404).json({ error: "not found sorry" })); //backup route
+app.use("*", (req, res) =>
+  res.status(404).json({ error: "Routeme not found sorry" })
+); //backup route
 
 export default app; //allows importing app
