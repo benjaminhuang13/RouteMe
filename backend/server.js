ac710b6424ddf1ac1a400a202a9dfe1eb124b9db
Benjamin Huang <benjamin.huang@alumni.stonybrook.edu>
2024-10-29 00:40:30 -0400

    fix health order

--- backend/server.js
+++ backend/server.js
@@ -3,17 +3,15 @@ import cors from "cors";
 import customer_data from "./api/customer_data.route.js";
 
 const app = express(); //load express into
-
+app.get("/health", (req, res) => {
+  res.status(200).send("Ok");
+  console.log("Health checked!");
+});
 app.use(cors()); //use middleware
 app.use(express.json()); //allows server to accept json in a body of a request
-
 app.use("/api/v1/customer_data", customer_data); //url of the routes
 app.use("*", (req, res) =>
   res.status(404).json({ error: "Routeme not found sorry" })
 ); //backup route
-
-app.get("/health", (req, res) => {
-  res.status(200).send("Ok");
-});
 
 export default app; //allows importing app
