4d60735b85d7c16bdb13beb0cded09d37ecc473a
Benjamin Huang <benjamin.huang@alumni.stonybrook.edu>
2024-11-19 17:22:38 -0500

    fix bug

--- postCustomerFunction/lambda_function.py
+++ postCustomerFunction/lambda_function.py
@@ -22,14 +22,14 @@ test_record= {'name': 'testtest',
         'long': 88,}
 
 
-test_message = {'Records': [{'messageId': 'd5ebb291-904d-4f8f-bbad-7533cbb71267', 'receiptHandle': 'AQEBmZ5l7vY26jsfFYZY8SFvE1iv9cZhDppbXDPTWiz0qenbtQr6E6syMemVikx/xI7/V/H2fpJmjYQ+tbrzNa3xGbL4nQe+pzrjEA4fBtYcrwAMk3FMkaesxyPMy8nS5NjaQhjV+pY7iHbOZVw8K6v/H/0b0QycOiZaDHD982VD+ohYuILQDazrdMB82mvNXssKaZDznvw9oes2DbzcKgkiBrVdCoytezHJsxoG59KIwnkdfp710Fld1A4frdoevYXvu20IcNUh7Bqkede2StJIR/u/INPjQpp6Uy0+/r7FZjpO5IPn9FVOzVW0ao3WeVNivBeobK5XmMYW+boxS4s8eSRiPyrcaPlzlz61oBEA6MDAf8jpcnllxrho4T1FIvJAfXKAb6qPtThXe4HRYWhpBg==', 'body': "{'name': 'testtest',\n        'street_addr': 'test',\n        'city': 'New York',\n        'state': 'NY',\n        'zip': 11355,\n        'country': 'US',\n        'lat': 88,\n        'long': 88,}", 'attributes': {'ApproximateReceiveCount': '2', 'SentTimestamp': '1732054214046', 'SenderId': '471112517107', 'ApproximateFirstReceiveTimestamp': '1732054214057'}, 'messageAttributes': {}, 'md5OfBody': 'ef266e9ff8d2a51c3fd6df4687c3755b', 'eventSource': 'aws:sqs', 'eventSourceARN': 'arn:aws:sqs:us-east-1:471112517107:post_customer_queue', 'awsRegion': 'us-east-1'}]}
+test_message = {'Records': [{'messageId': 'c863054d-1067-486f-bb2f-a2c034ad68c9', 'receiptHandle': 'AQEBGj6ZjPFCt6HczgS0Py6fnRCb3hDCezXGVMSsaQZaodqCGFP6UWlNGu5Jn4BXHgzS7lAMM4wbjXlDMZYnrdKcWmX2HWCjPAg7k5YpXTVPANnQ0Ds7XRHcCs/lqezjXNHvpkxJpwXst2fJUlHMAeEDae6uQX0onDJfbkbbr/71+GzZ12qmt1dxSNh8em9jY02p5brUuQCP3UfzkElS7lxi5u/8BwRr71jLT+tqLTMyvDEuk76/677YVE4I0kDaIzDom4Uwi4GW1+vOcUlnXvZdpvNUaew05rIpcfovW5ThI//9pMKRxXZqTBcnMoaqUj7v7cgEug6RLMtwInUm72DPj4W0IGvuaYOlQVOBXIdBviklsABVhi4q2CG7AZlFUR1RFiv9r4q2Pu+xpcDM8FMzWg==', 'body': '{"name": "testtest",\n"street_addr": "test",\n"city": "New York",\n"state": "NY",\n"zip": 11355,\n"country": "US",\n"lat": 88,\n"long": 88}', 'attributes': {'ApproximateReceiveCount': '2', 'SentTimestamp': '1732054792440', 'SenderId': '471112517107', 'ApproximateFirstReceiveTimestamp': '1732054792441'}, 'messageAttributes': {}, 'md5OfBody': '29dc1e8d6b4686988a1df9c622182fe2', 'eventSource': 'aws:sqs', 'eventSourceARN': 'arn:aws:sqs:us-east-1:471112517107:post_customer_queue', 'awsRegion': 'us-east-1'}]}
 
 def lambda_handler(event, context):
     print(event)
     if(event):
-        for r in event['Records']:
-            record = json.loads(r['body'])
-            print("Processing event name {}\n {}".format(record['name'],{r}))
+        for r in event["Records"]:
+            record = json.loads(r["body"])
+            print("Processing event name {}\n {}".format(record["name"],r))
 
             response = collection.insert_one(record)
             if response.acknowledged:
@@ -45,3 +45,5 @@ def lambda_handler(event, context):
         'headers': {'Content-Type': 'application/json'},
         'body': json.dumps('Execution success')
     }
+
+#lambda_handler(test_message,"")     # testing
